ea43805b305438279d04a535fd2276a70971a3db
Bohdan Vasylyk <bogdanvasilik2120@gmail.com>
2023-01-14 22:35:50 +0200

    first and second tasks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,21 @@
+import userData from 'Data/user.json';
+import statsData from 'Data/data.json';
+
+import Profile from './Profile/Profile';
+import Statistics from './Statistics/Statistics';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Profile
+        username={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        avatar={userData.avatar}
+        stats={userData.stats}
+      />
+
+      <Statistics title="Upload stats" stats={statsData} />
     </div>
   );
 };
